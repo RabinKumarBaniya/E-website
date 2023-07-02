0a6dab06aad1aa5d0cbf79f35719f5a411d3d1e4
rabinkumarbaniya <rabinkrbaniya09@gmail.com>
2023-07-02 10:02:37 +0545

    Slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Slider from "./components/Slider/Slider";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Slider />
     </div>
   );
 }
